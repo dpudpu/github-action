c0fef880bd01e44174f0f7bc9abc1d641481f6a6
dpudpu <dpudpu11@gmail.com>
2020-05-11 01:45:49 +0900

    user

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class User {
+    public int a(){
+        return 1;
+    }
+
+    public int b(){
+        return 2;
+    }
+
+    public int c() {
+        return 3;
+    }
+}
---
+++ src/test/java/com/example/demo/UserTest.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+    private final User user = new User();
+
+    @Test
+    void aTest() {
+        assertThat(user.a()).isEqualTo(1);
+    }
+
+    @Test
+    void bTest() {
+        assertThat(user.b()).isEqualTo(2);
+    }
+
+}
